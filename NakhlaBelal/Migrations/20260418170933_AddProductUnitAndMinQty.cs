c24d2438b1b88e9b01e99705fccd0b7681e519b4
Belalalimam <115436787+Belalalimam@users.noreply.github.com>
2026-04-18 19:15:25 +0200

    oo

---
+++ NakhlaBelal/Migrations/20260418170933_AddProductUnitAndMinQty.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NakhlaBelal.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductUnitAndMinQty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MinimumQuantity",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "QuantityStep",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Unit",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MinimumQuantity",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "QuantityStep",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Unit",
+                table: "Products");
+        }
+    }
+}
